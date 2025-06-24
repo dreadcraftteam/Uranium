87083765103959deab6d25bda997c89e9d14d5a5
dreadcraftteam <dreadcraftteam@gmail.com>
2025-06-24 23:58:17 +0500

    Add Basic Dynamic Library Loading
    hmm

--- engine/engine.c
+++ engine/engine.c
@@ -1,3 +1,14 @@
 // This is the main file in the Engine project.
 // By DREADCRAFT, June 2025
 //
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int engine_main()
+{
+    printf("Hello from engine!");
+}
+
+
--- launcher/launcher.c
+++ launcher/launcher.c
@@ -4,9 +4,35 @@
 
 #include <stdio.h>
 
-int main(int argc, char* argv[])
-{
-    printf("Hello World!");
+#include <dlfcn.h>
 
+int main()
+{
+    void *handle;
+    void (*engine_main)();
+    char *error;
+
+    // Open .SO file...
+    handle = dlopen("./engine.so", RTLD_LAZY);
+    if (!handle) 
+    {
+        printf("Failed to load .SO file! Programmer is Idiot!");
+        return 1;
+    }
+
+    // Load 'engine_main'...
+    engine_main = dlsym(handle, "engine_main");
+    if ((error = dlerror()) != NULL)
+    {
+        printf("Failed to load 'engine_main'! Programmer is Idiot!");
+        return 1;
+    }
+
+    // Call functions from .SO file...
+    engine_main();
+
+    // MEMORY LEAK EAAEAEAAAA!!@!!!11!!
+    dlclose(handle);
+   
     return 0;
 }
