c26e727a8e12a7bc7ba45a3dc349873008b874c8
dreadcraftteam <dreadcraftteam@gmail.com>
2025-06-26 04:16:08 +0500

    Add Window on SDL
    And small changes.

--- engine/engine.c
+++ engine/engine.c
@@ -16,6 +16,60 @@ int engine_main()
 {
     /* Call function for game load */
     loadGame();
+    
+    /* Window on SDL (Simple DirectMedia Layer) */
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        return 1;
+    }
+
+    SDL_Window* frame = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+    if (!frame)
+    {
+        SDL_Quit();
+       
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(frame, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        SDL_DestroyWindow(frame);
+        SDL_Quit();
+   
+        return 1;
+    }
+
+    SDL_Event event;
+    
+    /* Cycle */
+    while (running)
+    {
+        while (SDL_PollEvent(&event)) 
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+            
+            /* Close the window */
+            else if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE)
+                {
+                    running = false;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(frame);
+    SDL_Quit();
 
     return 0;
 }
