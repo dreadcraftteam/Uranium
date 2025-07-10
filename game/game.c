b707a9e57caa76db8770e4285f8e2a8a92e0a309
dreadcraftteam <dreadcraftteam@gmail.com>
2025-07-10 16:36:03 +0500

    Some changes

--- game/game.c
+++ game/game.c
@@ -5,22 +5,7 @@
 #include "stdio.h"
 #include "stdbool.h"
 
-#include "GL/glew.h"
-#include "GL/gl.h"
-
-/* Rotation cube Variables */
-float rot_x = 0.0f;
-float rot_y = 0.0f;
-float zoom = -3.5f;
-float cube_sizeX = 1.0f;
-float cube_sizeY = 1.0f; 
-float cube_sizeZ = 1.0f;
-
-bool mouse_down = false;
-bool cursor_hidden = false;
-
-int last_x = 0; 
-int last_y = 0;
+#include "model/cube.h"
 
 /* Main method for engine project */
 int game_main()
@@ -28,53 +13,4 @@ int game_main()
     drawCube();
 
     return 0;
-}
-
-void drawCube()
-{
-    glBegin(GL_QUADS);
-
-        /* Front face (red) */
-        glColor3f(1.0f, 0.0f, 0.0f);
-        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
-        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        
-        /* Back face (green) */
-        glColor3f(0.0f, 1.0f, 0.0f);
-        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        
-        /* Top face (blue) */
-        glColor3f(0.0f, 0.0f, 1.0f);
-        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
-        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
-        
-        /* Bottom face (yellow) */
-        glColor3f(1.0f, 1.0f, 0.0f);
-        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
-        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
-        
-        /* Right face (magenta) */
-        glColor3f(1.0f, 0.0f, 1.0f);
-        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
-        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
-
-        /* Left face (cyan) */
-        glColor3f(0.0f, 1.0f, 1.0f);
-        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
-        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
-        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
-        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
-   
-    glEnd();
 }
---
+++ game/model/cube.c
@@ -0,0 +1,61 @@
+// Cube Model
+// By DREADCRAFT
+//
+
+#include "stdio.h"
+#include "stdbool.h"
+
+#include "GL/glew.h"
+#include "GL/gl.h"
+
+#include "cube.h"
+
+/* yeah... */
+void drawCube()
+{
+    glBegin(GL_QUADS);
+
+        /* Front face (red) */
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
+        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        
+        /* Back face (green) */
+        glColor3f(0.0f, 1.0f, 0.0f);
+        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        
+        /* Top face (blue) */
+        glColor3f(0.0f, 0.0f, 1.0f);
+        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
+        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
+        
+        /* Bottom face (yellow) */
+        glColor3f(1.0f, 1.0f, 0.0f);
+        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
+        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
+        
+        /* Right face (magenta) */
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glVertex3f( cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY, -cube_sizeZ);
+        glVertex3f( cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        glVertex3f( cube_sizeX, -cube_sizeY,  cube_sizeZ);
+
+        /* Left face (cyan) */
+        glColor3f(0.0f, 1.0f, 1.0f);
+        glVertex3f(-cube_sizeX, -cube_sizeY, -cube_sizeZ);
+        glVertex3f(-cube_sizeX, -cube_sizeY,  cube_sizeZ);
+        glVertex3f(-cube_sizeX,  cube_sizeY,  cube_sizeZ);
+        glVertex3f(-cube_sizeX,  cube_sizeY, -cube_sizeZ);
+   
+    glEnd();
+}
