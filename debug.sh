33fae888ef8aacb848c0a82476952d44913a4342
dreadcraftteam <dreadcraftteam@gmail.com>
2025-07-14 18:22:36 +0500

    Small Fixes and Changes

--- debug.sh
+++ debug.sh
@@ -3,14 +3,14 @@
 YELLOW='\033[1;33m'
 NC='\033[0m' 
 
-if [ ! -d "cmake" ]; then
+if [ ! -d ".cmake" ]; then
     echo -e "${YELLOW}Build folder Not Found! Configuring Project...${NC}"
-    cmake -B cmake
+    cmake -B .cmake
 else
     echo -e "${YELLOW}Build folder Found! Building Project!${NC}"
 fi
 
 echo -e "${YELLOW}Building Project...${NC}"
-cmake --build cmake --config Debug
+cmake --build .cmake --config Debug
 
 echo -e "${YELLOW}Build Complete!${NC}"
